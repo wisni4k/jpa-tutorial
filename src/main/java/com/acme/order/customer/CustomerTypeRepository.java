9cb8e808b281e1a2f19808d93208d739db204c31
wisni4k <mateusz.wisniewski0@gmail.com>
2015-07-09 15:29:16 +0200

    Jakies zmiany

--- src/main/java/com/acme/order/customer/CustomerTypeRepository.java
+++ src/main/java/com/acme/order/customer/CustomerTypeRepository.java
@@ -1,9 +1,7 @@
 package com.acme.order.customer;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.acme.order.pizza.PizzaOrder;
-
-public interface CustomerTypeRepository extends CrudRepository<CustomerType, Long> {
+public interface CustomerTypeRepository extends JpaRepository<CustomerType, Long> {
 
 }
